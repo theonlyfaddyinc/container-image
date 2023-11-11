050dcdff41fb409b2362cac5b418f623ba435921
theonlyfaddy <121252445+theonlyfaddy@users.noreply.github.com>
2023-11-11 10:44:47 -0500

    Update entrypoint.sh

--- github-actions-runner/entrypoint.sh
+++ github-actions-runner/entrypoint.sh
@@ -4,12 +4,19 @@ echo "GITHUB_PAT=$GITHUB_PAT"
 echo "REGISTRATION_TOKEN_API_URL=$REGISTRATION_TOKEN_API_URL"
 echo "Curl=curl -X POST -fsSL -H 'Accept: application/vnd.github.v3+json'  -H \"Authorization: Bearer $GITHUB_PAT\"  -H 'X-GitHub-Api-Version: 2022-11-28' \"$REGISTRATION_TOKEN_API_URL\" jq -r '.token')"
 # Retrieve a short lived runner registration token using the PAT
-REGISTRATION_TOKEN="$(curl -X POST -fsSL \
-  -H 'Accept: application/vnd.github.v3+json' \
+REGISTRATION_TOKEN="$(curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_PAT" \
-  -H 'X-GitHub-Api-Version: 2022-11-28' \
-  "$REGISTRATION_TOKEN_API_URL" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "https://api.github.com/repos/theonlyfaddyinc/my-second-repo/actions/runners/registration-token" \
   | jq -r '.token')"
+# $(curl -X POST -fsSL \
+#   -H 'Accept: application/vnd.github.v3+json' \
+#   -H "Authorization: Bearer $GITHUB_PAT" \
+#   -H 'X-GitHub-Api-Version: 2022-11-28' \
+#   "$REGISTRATION_TOKEN_API_URL" \
+  
 
   
 
